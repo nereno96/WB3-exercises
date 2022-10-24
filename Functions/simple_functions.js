b6157a3d4406901b842cda6f8254873e718f8221
Najee Reno <nereno96@gmail.com>
2022-10-24 13:13:29 -0400

    completed simple_functions.js exercise

---
+++ Functions/simple_functions.js
@@ -0,0 +1,23 @@
+function favoriteThings() {
+console.log("Najee");
+console.log("Over The Hedge");
+console.log("Burnout Syndromes");
+}
+
+favoriteThings();
+
+//..........................
+
+function whyImHere() {
+    console.log("I'm here to learn how to code.");
+}
+
+whyImHere();
+
+//..........................
+
+function favoritePlace() {
+    console.log("My favorite place to visit is Barnes and Noble. Going to big bookstores is always a relaxing experience.");
+}
+
+favoritePlace();
