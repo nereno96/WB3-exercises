6442b0ca079df84aa1dc7eba79ee1ab821a44624
Najee Reno <nereno96@gmail.com>
2022-10-24 14:06:16 -0400

    completed f_to_c.js exercise

---
+++ Functions/f_to_c.js
@@ -0,0 +1,42 @@
+function convertFtoC(f) {
+   let c = (f - 32) * (5 / 9);
+    return c;
+}
+
+let currentTemp1 = 92;
+let celciusTemp1 = convertFtoC(currentTemp1);
+
+console.log(celciusTemp1 + " ºC")
+
+let currentTemp2 = 212;
+let celciusTemp2 = convertFtoC(currentTemp2);
+
+console.log(celciusTemp2 + " ºC")
+
+let currentTemp3 = 90;
+let celciusTemp3 = convertFtoC(currentTemp3);
+
+console.log(celciusTemp3 + " ºC")
+
+let currentTemp4 = 72;
+let celciusTemp4 = convertFtoC(currentTemp4);
+
+console.log(celciusTemp4 + " ºC")
+
+let currentTemp5 = 32;
+let celciusTemp5 = convertFtoC(currentTemp5);
+
+console.log(celciusTemp5 + " ºC")
+
+let currentTemp6 = 0;
+let celciusTemp6 = convertFtoC(currentTemp6);
+
+console.log(celciusTemp6 + " ºC")
+
+let currentTemp7 = -40;
+let celciusTemp7 = convertFtoC(currentTemp7);
+
+console.log(celciusTemp7 + " ºC")
+
+//..........................
+
