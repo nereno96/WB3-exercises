a905bd94313719e69b09641e2c2e53cd7d665114
Najee Reno <nereno96@gmail.com>
2022-10-24 13:44:01 -0400

    completed more_functions.js exercise

---
+++ Functions/more_functions.js
@@ -0,0 +1,44 @@
+function displayMailingLabel(name, address, city, state, zip) {
+    console.log(name);
+    console.log(address);
+    console.log(city + ",  " + state + " " + zip);
+}
+
+let name = "Stacy Preston";
+let address = "1845 South Union Ave.";
+let city = "Bangor";
+let state = "Maine";
+let zip = "04403";
+
+displayMailingLabel(name, address, city, state, zip);
+displayMailingLabel("Legolas", "100", "Middle", "Earth", "00000");
+
+//..........................
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    console.log(num1 + " + " + num2 + " = " + sum);
+}
+
+let num1 = 20;
+let num2 = 1738;
+
+addNumbers(num1, num2);
+addNumbers(8, 12);
+
+//..........................
+
+function displayReceipt(totalDue, amountPaid) {
+    let changeDue = amountPaid - totalDue;
+    console.log("Total Due: $" + totalDue);
+    console.log("Amount Paid: $" + amountPaid);
+    console.log("")
+    console.log("Change Due: $" + changeDue);
+}
+
+    let totalDue = 14;
+    let amountPaid = 20;
+
+    displayReceipt(totalDue, amountPaid);
+    displayReceipt(20, 20);
+    displayReceipt(20, 14);
