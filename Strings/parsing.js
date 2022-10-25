ecf149632910323d45a2516dcd8481c309bf01a9
Najee Reno <nereno96@gmail.com>
2022-10-25 11:36:53 -0400

    completed parsing.js exercise

--- Strings/parsing.js
+++ Strings/parsing.js
@@ -1,8 +1,6 @@
 "use strict"
 
-let name = "Tiara Edwards";
-
-
+function parseAndDisplayName(name){
 let pos = name.indexOf(" ");
 let firstName = name.substring(0, pos);
 let lastName = name.substring(pos);
@@ -10,3 +8,11 @@ let lastName = name.substring(pos);
 console.log("Name: " + name);
 console.log("First name: " + firstName);
 console.log("Last name: " + lastName);
+console.log("");
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
+
+
